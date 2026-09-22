a5828078a7da9af5b034e338cef8944ece48c314
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-09-23 01:38:26 +0600

    Implement user management with CRUD operations and health check endpoint

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type User struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
+var users = []User{
+	{
+		Id:    1,
+		Name:  "Towsif",
+		Age:   23,
+		Email: "towsif@gmail.com",
+	},
+	{
+		Id:    2,
+		Name:  "Abdullah",
+		Age:   24,
+		Email: "abdullah@gmail.com",
+	},
+}
+
 func main() {
-	http.HandleFunc("/", rootHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", rootHandler)
+	mux.HandleFunc("GET /health", healthHandler)
+	mux.HandleFunc("POST /user", createUsersHandler)
+	mux.HandleFunc("GET /users", getUsersHandler)
 
 	fmt.Println("Server is running on port 5000")
-	err := http.ListenAndServe(":5000", nil)
+	err := http.ListenAndServe(":5000", mux)
 
 	if err != nil {
 		fmt.Println("Server error:", err)
@@ -18,4 +45,34 @@ func main() {
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to go server!")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Server is up and running!")
+}
+
+func createUsersHandler(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid request body")
+		return
+	}
+	newUser.Id = len(users) + 1
+	users = append(users, newUser)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newUser)
+}
+
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// users, _ := json.Marshal(users) // converts the data into json after saving into the memory
+	// w.Write(users)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(users)
 }
